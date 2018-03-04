63ac7e0d7531112bc778605dca19e9f8812d81e7
Michael Ndjinga <michael.ndjinga@cea.fr>
2018-03-04 21:05:36 +0100

    Updated env_CoreFlows.sh to synchronise it permanently to CDMATH

--- env_CoreFlows.sh
+++ env_CoreFlows.sh
@@ -5,8 +5,10 @@ export CDMATH_DIR=@CDMATH_DIR@
 export PETSC_DIR=@PETSC_DIR@
 export PETSC_ARCH="@PETSC_ARCH@"
 
+source $CDMATH_DIR/env_CDMATH.sh
+
 #------------------------------------------------------------------------------------------------------------------- 
 export CoreFlows=$CoreFlows_INSTALL/bin/Executable/CoreFlowsMainExe
-export LD_LIBRARY_PATH=$CDMATH_DIR/lib/:$CDMATH_DIR/lib/medcoupling/:$CDMATH_DIR/lib/med/:$PETSC_DIR/$PETSC_ARCH/lib:${PETSC_DIR}/lib:/usr/lib64/:$CoreFlows_INSTALL/lib:${LD_LIBRARY_PATH}
-export PYTHONPATH=$CoreFlows_INSTALL/lib:$CoreFlows_INSTALL/lib/CoreFlows_Python:$CoreFlows_INSTALL/bin/CoreFlows_Python:$CoreFlows_INSTALL/lib/python2.7/site-packages/salome:$CDMATH_DIR/lib/:$CDMATH_DIR/lib/cdmath:$CDMATH_DIR/bin/cdmath:$CDMATH_INSTALL/bin/cdmath/postprocessing:$CDMATH_INSTALL/lib/medcoupling/python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}/site-packages:${PYTHONPATH}
+export LD_LIBRARY_PATH=$PETSC_DIR/$PETSC_ARCH/lib:${PETSC_DIR}/lib:/usr/lib64/:$CoreFlows_INSTALL/lib:${LD_LIBRARY_PATH}
+export PYTHONPATH=$CoreFlows_INSTALL/lib:$CoreFlows_INSTALL/lib/CoreFlows_Python:$CoreFlows_INSTALL/bin/CoreFlows_Python:$CoreFlows_INSTALL/lib/python2.7/site-packages/salome:${PYTHONPATH}
 export CoreFlowsGUI=$CoreFlows_INSTALL/bin/salome/CoreFlows_Standalone.py
