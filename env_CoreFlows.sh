65ba8e60822edcd1b43e2cabc42715aee7f0e95e
Michael NDJINGA <ndjinga@localhost.localdomain>
2019-07-26 18:39:59 +0200

    Added possibility to set petsc variable for system petsc package

--- env_CoreFlows.sh
+++ env_CoreFlows.sh
@@ -3,12 +3,14 @@
 export CoreFlows_INSTALL=@CMAKE_INSTALL_PREFIX@
 export CDMATH_DIR=@CDMATH_DIR@
 export PETSC_DIR=@PETSC_DIR@
-export PETSC_ARCH="@PETSC_ARCH@"
+export PETSC_ARCH=@PETSC_ARCH@
+export PETSC_INCLUDES=@PETSC_INCLUDES@
+export PETSC_LIBRARIES=@PETSC_LIBRARIES@
 
 source $CDMATH_DIR/env_CDMATH.sh
 
 #------------------------------------------------------------------------------------------------------------------- 
 export CoreFlows=$CoreFlows_INSTALL/bin/Executable/CoreFlowsMainExe
-export LD_LIBRARY_PATH=$PETSC_DIR/$PETSC_ARCH/lib:${PETSC_DIR}/lib:/usr/lib64/:$CoreFlows_INSTALL/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=$PETSC_DIR/$PETSC_ARCH/lib:${PETSC_DIR}/lib:/usr/lib64/:$CoreFlows_INSTALL/lib:$PETSC_LIBRARIES:${LD_LIBRARY_PATH}
 export PYTHONPATH=$CoreFlows_INSTALL/lib:$CoreFlows_INSTALL/lib/CoreFlows_Python:$CoreFlows_INSTALL/bin/CoreFlows_Python:$CoreFlows_INSTALL/lib/python2.7/site-packages/salome:${PYTHONPATH}
 export CoreFlowsGUI=$CoreFlows_INSTALL/bin/salome/CoreFlows_Standalone.py
