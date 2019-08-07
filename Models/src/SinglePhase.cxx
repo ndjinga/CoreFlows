75ab710227c2655cdaf7378659c3086be95299ce
Michael Ndjinga <michael.ndjinga@cea.fr>
2019-08-07 13:11:54 +0200

    Improved comments and warning messages

--- Models/src/SinglePhase.cxx
+++ Models/src/SinglePhase.cxx
@@ -620,9 +620,11 @@ void SinglePhase::setBoundaryState(string nameOfGroup, const int &j,double *norm
 		VecAssemblyEnd(_Uextdiff);
 	}
 	else if (_limitField[nameOfGroup].bcType==Outlet){
-		if(q_n<=0 &&  _nbTimeStep%_freqSave ==0)
-			cout<< "Warning : fluid going in through outlet boundary "<<nameOfGroup<<". Applying Neumann boundary condition for velocity and temperature"<<endl;
-
+		if(q_n < -_precision &&  _nbTimeStep%_freqSave ==0)
+        {
+			cout<< "Warning : fluid going in through outlet boundary "<<nameOfGroup<<" with flow rate "<< q_n<<endl;
+            cout<< "Applying Neumann boundary condition for velocity and temperature"<<endl;
+        }
 		//Computation of the hydrostatic contribution : scalar product between gravity vector and position vector
 		Cell Cj=_mesh.getCell(j);
 		double hydroPress=Cj.x()*_GravityField3d[0];
